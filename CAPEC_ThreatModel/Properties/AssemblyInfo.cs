f59040ca76c9831661614d7944ed9d2399db50cc
Jerome Athias <athiasjerome@gmail.com>
2015-03-01 17:14:54 +0100

    Added CAPEC_ThreatModel
    Tool for generating generic Threat Models based on MITRE CAPEC Attack
    Patterns. The tool uses an XORCISM database. Note: use the tool
    Import_CAPEC before this one

---
+++ CAPEC_ThreatModel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CAPEC_ThreatModel")]
+[assembly: AssemblyDescription("Generates Threat Model based on MITRE CAPEC Attack Patterns library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CAPEC_ThreatModel")]
+[assembly: AssemblyCopyright("Copyright © Jerome Athias 2014-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("097d0d82-b532-4dd6-b688-25923816c99a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
